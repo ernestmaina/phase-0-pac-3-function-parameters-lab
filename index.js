f3e9478cbc93cb0673e07f09ceabd95f3ddcb7a7
ErnestMaina <ernestmaina@student.moringaschool.com>
2023-09-13 22:29:12 +0300

    Done

---
+++ index.js
@@ -0,0 +1,17 @@
+function introductionWithLanguage(name, language) {
+    console.log(`Hi,my name is ${Ernest} and i am learning to program in ${Java}.`);
+    
+  }
+
+  function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+
+  function introductionWithLanguageOptional(name, language) {
+    
+    console.log(`Hi,my name is ${Ernest} and i am learning to program in ${JavaScript}.`);
+  }
+  
+
+  
